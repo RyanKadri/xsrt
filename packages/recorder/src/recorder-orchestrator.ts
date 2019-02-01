79b45d241d219348f98310f4fb79551c514fda1d
Ryan Kadri <ryankadri1@gmail.com>
2019-02-01 11:18:42 -0500

    Pushing some changes

--- packages/recorder/src/recorder-orchestrator.ts
+++ packages/recorder/src/recorder-orchestrator.ts
@@ -29,7 +29,7 @@ export class RecorderOrchestrator {
     private sentInitChunk = false;
 
     initialize() {
-
+        this.logger.info("Initializing");
         this.recorder.record();
         this.initInfoTask = this.recorderApi.startRecording()
             .then(info => this.initInfo = info);
