55ecc9ac6ba1ec2c10ee5468b08d17266d20eaf7
Ryan Kadri <ryankadri1@gmail.com>
2019-02-14 12:32:01 -0500

    Fixing issue with test compilation

--- spec/support/init.js
+++ spec/support/init.js
@@ -1,3 +1,5 @@
+const moduleAlias = require("module-alias");
+moduleAlias.addAlias("@xsrt/common", __dirname + "../../../packages/common/src")
 require('reflect-metadata');
 require('ts-node').register({ project: './tsconfig.spec.json', transpileOnly: true })
 require('source-map-support/register')
