e8a56cac956564c01007af4a7d7fac189fe099c2
johndoe@example.com <johndoe@example.com>
2020-06-24 09:21:02 -0400

    Trying to update recorder build

---
+++ packages/recorder/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+  ],
+  plugins: [
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }]
+  ]
+};
